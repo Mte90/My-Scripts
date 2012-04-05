de70e83eafdc4fdc6818275ccb2b023b7d95b0a1
Mte90 <mte90smanettator@gmail.com>
2012-04-05 16:19:57 +0200

    Addon Gimp, fix vari, pulizia

--- preparatore.sh
+++ preparatore.sh
@@ -5,6 +5,7 @@ echo "Preparatore per Aptosid 1.3 by Mte90 - www.mte90.net"
 UTENTE="mte90"
 cd /home/$UTENTE/Desktop
 
+echo "Aggiunta Repo"
 echo "deb http://www.debian-multimedia.org sid main non-free
 deb http://backports.debian.org/debian-backports lenny-backports main contrib non-free
 deb http://ftp.bononia.it/debian/ experimental main contrib non-free
@@ -17,8 +18,9 @@ apt-key add jcameron-key.asc
 rm ./jcameron-key.asc
 
 #sfondi
+#script da github
 
-echo "Repo aggiunti"
+echo "Fix & tips"
 
 echo 'CONCURRENCY=makefile' >> /etc/default/rcS
 
@@ -31,6 +33,7 @@ alias update='apt-get update'
 alias upgrade='apt-get upgrade'
 alias search='apt-cache search'
 alias deb64='dpkg --force-architecture -i'
+alias policy='apt-cache policy'
 alias casa='cd /home/$UTENTE/Desktop'
 
 echo -e '\e[1;31m';
@@ -47,6 +50,7 @@ alias renamemp3='eyeD3 --rename=\"%A - %t\" ./*'
 
 dpkg-reconfigure locales
 
+echo "Aggiornamento ed installazione programmi"
 apt-get update
 apt-get remove kde-l10n-de kaffeine aptosid-manual* aptosid-irc install-usb-gui gparted bluez bluez-pcmcia-support
 apt-get -y install kde-l10n-it debian-multimedia-keyring
@@ -66,7 +70,8 @@ apt-get install emesene amule amule-daemon amule-utils plasma-widget-amule delug
 #Ufficio
 apt-get install libreoffice-writer libreoffice-l10n-it libreoffice-kde libreoffice-impress libreoffice-calc libreoffice-draw tellico korganizer
 #Mozilla/Chromium :-(
-apt-get install myspell-it mozilla-libreoffice mozplugger flashplugin-nonfree chromium-browser mozilla-plugin-vlc iceweasel iceweasel-l10n-it
+apt-get install -t experimental iceweasel iceweasel-l10n-it
+apt-get install myspell-it mozilla-libreoffice mozplugger flashplugin-nonfree chromium-browser mozilla-plugin-vlc
 #Sistema
 apt-get install update-notifier-kde kde-config-gtk-style apt-rdepends webmin imwheel gtk2-engines-pixbuf gtk2-engines-oxygen file-roller bum acetoneiso virtualbox virtualbox-ose-qt virtualbox-dkms qt4-qmake
 #Programmazione
@@ -79,6 +84,7 @@ apt-get install ttf-mscorefonts-installer ttf-droid ttf-dejavu ttf-freefont ttf-
 apt-get install assaultcube
 apt-get clean
 
+echo "Fix"
 modprobe vboxdrv
 modprobe vboxnetflt
 a2enmod rewrite
@@ -93,15 +99,16 @@ while true; do
     esac
 done
 
+mkdir install_
+cd ./install_
+
 echo "
 installo BeShadowed"
-wget -O beshadowed.txz http://kde-apps.org/CONTENT/content-files/121607-beshadowed-kwin-fx-0.8a.txz
+wget -b -O beshadowed.txz http://kde-apps.org/CONTENT/content-files/121607-beshadowed-kwin-fx-0.8a.txz
 tar Jxvf ./beshadowed.txz
 cd ./beshadowed-kwin-fx && ./configure
 cd build; make && make install
-cd /home/$UTENTE/Desktop
-rm ./beshadowed.txz
-rm -r ./beshadowed-kwin-fx
+cd ../
 
 while true; do
     read -n 1 -p "Vuoi proseguire?" sn
@@ -114,17 +121,14 @@ done
 
 echo "
 Installo qualche plasmoide"
-wget http://download.opensuse.org/repositories/home:/pinters/Debian_6.0/amd64/netspeed-plasma_0.2-1_amd64.deb
+wget -b http://download.opensuse.org/repositories/home:/pinters/Debian_6.0/amd64/netspeed-plasma_0.2-1_amd64.deb
 dpkg -i ./netspeed-plasma_0.2-1_amd64.deb
-rm ./netspeed-plasma_0.2-1_amd64.deb
 
-wget -O playctrl.plasmoid http://kde-apps.org/CONTENT/content-files/144437-playctrl.plasmoid
+wget -b -O playctrl.plasmoid http://kde-apps.org/CONTENT/content-files/144437-playctrl.plasmoid
 plasmapkg -i ./playctrl.plasmoid
-rm ./playctrl.plasmoid
 
-wget -O appmenu.plasmoid http://kde-apps.org/CONTENT/content-files/146098-plasma-applet-appmenu-qml-0.7.2.plasmoid
-plasmapkg -i ./appmenu.plasmoid
-rm ./appmenu.plasmoid
+#wget -O appmenu.plasmoid http://kde-apps.org/CONTENT/content-files/146098-plasma-applet-appmenu-qml-0.7.2.plasmoid
+#plasmapkg -i ./appmenu.plasmoid
 
 while true; do
     read -n 1 -p "Vuoi proseguire?" sn
@@ -138,53 +142,43 @@ done
 echo "
 Qualche chicca"
 
-wget http://kde-apps.org/CONTENT/content-files/141946-MediaInfoKDE.desktop
+wget -b http://kde-apps.org/CONTENT/content-files/141946-MediaInfoKDE.desktop
 mv ./141946-MediaInfoKDE.desktop /usr/share/kde4/services/ServiceMenus/MediaInfoKDE.desktop
 
-wget http://download.tuxfamily.org/ramielinux/aMuleKollection/amuleKollection-0.4.tar.gz
+wget -b http://download.tuxfamily.org/ramielinux/aMuleKollection/amuleKollection-0.4.tar.gz
 tar zxvf ./amuleKollection-0.4.tar.gz
 cd ./amuleKollection-0.4 && ./install.sh
-cd /home/$UTENTE/Desktop
-rm ./amuleKollection-0.4.tar.gz
-rm -r ./amuleKollection-0.4
+cd ../
 
-wget http://dl.dropbox.com/u/23646728/BlaQuave.tar.gz
+wget -b http://dl.dropbox.com/u/23646728/BlaQuave.tar.gz
 tar zxvf ./BlaQuave.tar.gz
 mv ./BlaQuave /usr/share/icons/BlaQuave
-rm ./BlaQuave.tar.gz
 
-wget -O robots.xml http://kde-files.org/CONTENT/content-files/124969-robots.xml
+wget -b -O robots.xml http://kde-files.org/CONTENT/content-files/124969-robots.xml
 mv ./robots.xml /usr/share/apps/katepart/syntax/
 
 mkdir ./audiothumbs
 cd ./audiothumbs
-wget -O AudioThumbs-0.2.tar.gz http://kde-apps.org/CONTENT/content-files/145088-AudioThumbs-0.2.tar.gz
+wget -b -O AudioThumbs-0.2.tar.gz http://kde-apps.org/CONTENT/content-files/145088-AudioThumbs-0.2.tar.gz
 tar zxvf ./AudioThumbs-0.2.tar.gz
 mkdir ./build
 cd ./build
 cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` ..
-make
-make install
-rm ./AudioThumbs-0.2.tar.gz
-rm -r ./audiothumbs
-cd /home/$UTENTE/Desktop
+make && make install
+cd ../
 
-wget -O ofwvlc.tar.gz http://kde-apps.org/CONTENT/content-files/146621-ofwvlc-0.3.tar.gz
+wget -b -O ofwvlc.tar.gz http://kde-apps.org/CONTENT/content-files/146621-ofwvlc-0.3.tar.gz
 tar zxvf ./ofwvlc.tar.gz
 cd ./ofwvlc; cp vlc.desktop /usr/share/kde4/services/ServiceMenus/
 cd it; cp ofwvlc /usr/sbin
 chmod +x /usr/sbin/ofwvlc /usr/share/kde4/services/ServiceMenus/vlc.desktop
-cd /home/$UTENTE/Desktop
-rm ./ofwvlc.tar.gz
-rm -r ./ofwvlc
+cd ../
 
-wget -O kate-folder-service-menu.tar.gz http://kate-folder-service-menu.googlecode.com/files/kate-folder-service-menu0.2.tar.gz
+wget -b -O kate-folder-service-menu.tar.gz http://kate-folder-service-menu.googlecode.com/files/kate-folder-service-menu0.2.tar.gz
 tar zxvf ./kate-folder-service-menu.tar.gz
 cd ./kate-folder-service-menu
 ./install
-cd /home/$UTENTE/Desktop
-rm ./kate-folder-service-menu.tar.gz
-rm -r ./kate-folder-service-menu
+cd ../
 
 echo "
 Avvio FileZilla, Wine, Gimp per inizializzarli!"
@@ -205,23 +199,20 @@ done
 echo "
 Kdeizziamo FileZilla, Wine, Gimp"
 
-wget -O filezilla-theme.tar.gz http://kde-look.org/CONTENT/content-files/141546-filezilla-oxygen-theme.tar.gz
+wget -b -O filezilla-theme.tar.gz http://kde-look.org/CONTENT/content-files/141546-filezilla-oxygen-theme.tar.gz
 tar zxvf ./filezilla-theme.tar.gz
 mv ./oxygen /usr/share/filezilla/resources/oxygen
-rm ./filezilla-theme.tar.gz
 
-wget http://dl.dropbox.com/u/17620616/linki/Oxywine_3.1.msstyles
+wget -b http://dl.dropbox.com/u/17620616/linki/Oxywine_3.1.msstyles
 mkdir /home/$UTENTE/.wine/drive_c/windows/Resources
 mkdir /home/$UTENTE/.wine/drive_c/windows/Resources/Themes
 mkdir /home/$UTENTE/.wine/drive_c/windows/Resources/Themes/Oxywine_3.1
 chmod -R 777 /home/$UTENTE/.wine/drive_c/windows/Resources
 mv ./Oxywine_3.1.msstyles /home/$UTENTE/.wine/drive_c/windows/Resources/Themes/Oxywine_3.1
 
-wget -O gimp-refresh.zip http://kde-look.org/CONTENT/content-files/141457-GIMP-Refresh%200.1.zip
+wget -b -O gimp-refresh.zip http://kde-look.org/CONTENT/content-files/141457-GIMP-Refresh%200.1.zip
 unzip ./gimp-refresh.zip
 mv ./GIMP-Refresh /usr/share/gimp/2.0/theme/GIMP-Refresh
-rm -r ./GIMP-Refresh
-rm ./gimp-refresh.zip
 
 while true; do
     read -n 1 -p "Vuoi proseguire?" sn
@@ -235,7 +226,7 @@ done
 echo "
 installo i Google Font - Impiega diverso tempo"
 cd /home/$UTENTE/
-wget http://webupd8.googlecode.com/files/install-google-fonts
+wget -b http://webupd8.googlecode.com/files/install-google-fonts
 chmod +x install-google-fonts
 echo "Intanto devi settare Gimp, Filezilla e Wine per usare i temi scaricati, link per universalindentgui."
 
@@ -249,25 +240,22 @@ while true; do
 done
 
 ./install-google-fonts
-cd /home/$UTENTE/Desktop
+cd /home/$UTENTE/Desktop/install_
 
 echo "
 installo Grub2 KCM"
-wget http://ppa.launchpad.net/eu-andreduartesp/ppa/ubuntu/pool/main/k/kcmgrub2/kde-config-grub_1.1-ubuntu2_amd64.deb
+wget -b http://ppa.launchpad.net/eu-andreduartesp/ppa/ubuntu/pool/main/k/kcmgrub2/kde-config-grub_1.1-ubuntu2_amd64.deb
 dpkg -i ./kde-config-grub_1.1-ubuntu2_amd64.deb
-rm ./kde-config-grub_1.1-ubuntu2_amd64.deb
 
 echo "
 installo GTK Config KCM"
-wget http://chakra-project.org/sources/gtk-integration/chakra-gtk-config-1.7.tar.gz
+wget -b http://chakra-project.org/sources/gtk-integration/chakra-gtk-config-1.7.tar.gz
 tar zxvf ./chakra-gtk-config-1.7.tar.gz
 cd ./chakra-gtk-config-1.77
 mkdir build && cd build
 cmake ..
 make && make install
-cd /home/$UTENTE/Desktop
-rm ./chakra-gtk-config-1.7.tar.gz
-rm -r ./chakra-gtk-config-1.7
+cd ../
 
 while true; do
     read -n 1 -p "Vuoi proseguire?" sn
@@ -280,24 +268,20 @@ done
 
 echo "
 installo SVG Cleaner"
-wget -O svgcleaner.tar.gz https://github.com/RazrFalcon/SVGCleaner/tarball/master
+wget -b -O svgcleaner.tar.gz https://github.com/RazrFalcon/SVGCleaner/tarball/master
 tar zxvf ./svgcleaner.tar.gz
-cd ./RazrFalcon-SVGCleaner-91a759a
+cd `tar tzf svgcleaner.tar.gz | head -1`
 qmake
 make && make install
-cd /home/$UTENTE/Desktop
-rm ./svgcleaner.tar.gz
-rm -r ./RazrFalcon-SVGCleaner-5069fce
+cd ../
 
 echo "
 installo Krep"
-wget http://www.staerk.de/files/krep.tar.gz
+wget -b http://www.staerk.de/files/krep.tar.gz
 tar zxvf ./krep.tar.gz
 cd krep
 cmake . && make && make install
-cd /home/$UTENTE/Desktop
-rm ./krep.tar.gz
-rm -r ./krep
+cd ../
 
 while true; do
     read -n 1 -p "Vuoi proseguire?" sn
@@ -310,13 +294,11 @@ done
 
 echo "
 installo Converseen"
-wget http://sourceforge.net/projects/converseen/files/Converseen/Converseen%200.4/0.4.9/converseen-0.4.9.tar.bz2/download
+wget -b http://sourceforge.net/projects/converseen/files/Converseen/Converseen%200.4/0.4.9/converseen-0.4.9.tar.bz2/download
 tar jxvf ./converseen-0.4.9.tar.bz2
 cd ./converseen-0.4.9
 qmake && make && make install
-cd /home/$UTENTE/Desktop
-rm ./converseen-0.4.9.tar.bz2
-rm -r ./converseen-0.4.9
+cd ../
 
 while true; do
     read -n 1 -p "Vuoi proseguire?" sn
@@ -329,21 +311,18 @@ done
 
 echo "
 installo Gmic per Gimp"
-wget http://ignum.dl.sourceforge.net/project/gmic/gmic_gimp_linux64.zip
+wget -b http://ignum.dl.sourceforge.net/project/gmic/gmic_gimp_linux64.zip
 unzip ./gmic_gimp_linux64.zip
 mv ./gmic_gimp /usr/lib/gimp/2.0/plug-ins/
-rm ./gmic_gimp_linux64.zip
-rm ./README
 
 echo "
 installo la cagata di Skype"
-wget -O skype-install.deb http://www.skype.com/go/getskype-linux-deb-64
+wget -b -O skype-install.deb http://www.skype.com/go/getskype-linux-deb-64
 dpkg -i ./skype-install.deb
-rm ./skype-install.deb
 
 echo "
 Installiamo Orta come tema per GTK"
-wget http://www.deviantart.com/download/184118297/orta_by_skiesofazel-d31mal5.zip
+wget -b http://www.deviantart.com/download/184118297/orta_by_skiesofazel-d31mal5.zip
 unzip ./orta_by_skiesofazel-d31mal5.zip
 tar zxvf ./extractme.tar.gz
 cd ./extractme
@@ -364,7 +343,7 @@ cd /home/mte90
 svn co https://cloudcity.svn.sourceforge.net/svnroot/cloudcity
 cd cloudcity && ./configure
 cd build && make && make install
-cd /home/$UTENTE/Desktop
+cd /home/$UTENTE/Desktop/install_
 
 while true; do
     read -n 1 -p "Vuoi proseguire?" sn
@@ -398,18 +377,16 @@ echo "
 Installo Brother MFC620CN"
 #http://welcome.solutions.brother.com/bsc/public_s/id/linux/en/index.html
 #http://welcome.solutions.brother.com/bsc/public_s/id/linux/en/download_prn.html#MFC-620CN
-wget http://www.brother.com/pub/bsc/linux/dlf/mfc620cnlpr-1.0.2-1.i386.deb
-wget http://www.brother.com/pub/bsc/linux/dlf/cupswrapperMFC620CN-1.0.2-3.i386.deb
+wget -b http://www.brother.com/pub/bsc/linux/dlf/mfc620cnlpr-1.0.2-1.i386.deb
+wget -b http://www.brother.com/pub/bsc/linux/dlf/cupswrapperMFC620CN-1.0.2-3.i386.deb
 dpkg -i --force-architecture ./mfc620cnlpr-1.0.2-1.i386.deb
 dpkg -i --force-architecture ./cupswrapperMFC620CN-1.0.2-3.i386.deb
 #file fixato per i margini
-wget http://dl.dropbox.com/u/21763079/MFC620CN.ppd
+wget -b http://dl.dropbox.com/u/21763079/MFC620CN.ppd
 mv ./MFC620CN.ppd /etc/cups/ppd/MFC620CN.ppd
-rm ./mfc620cnlpr-1.0.2-1.i386.deb
-rm ./cupswrapperMFC620CN-1.0.2-3.i386.deb
 
 echo "Download HotKeys"
-wget -O preset.hotkeys http://kde-look.org/CONTENT/content-files/148793-preset.khotkeys
+wget -b -O preset.hotkeys http://kde-look.org/CONTENT/content-files/148793-preset.khotkeys
 
 su $UTENTE -c "kcmshell4 khotkeys"
 
@@ -424,30 +401,30 @@ su $UTENTE -c "bespin config"
 
 echo "
 Carico la configurazione di Samba"
-wget http://dl.dropbox.com/u/21763079/smb.conf
+wget -b http://dl.dropbox.com/u/21763079/smb.conf
 mv ./smb.conf /etc/samba/smb.conf
 
 echo "
 Carico la configurazione di Konversation"
 su $UTENTE -c "konversation"
-wget http://dl.dropbox.com/u/21763079/konversationrc
+wget -b http://dl.dropbox.com/u/21763079/konversationrc
 mv ./konversationrc /home/mte90/.kde/share/config/konversationrc
 
 echo "
 Carico la configurazione di Kate"
 su $UTENTE -c "kate"
-wget http://dl.dropbox.com/u/21763079/katerc
+wget -b http://dl.dropbox.com/u/21763079/katerc
 mv ./katerc /home/mte90/.kde/share/config/katerc
 
 echo "
 Carico la configurazione di Choqok"
-su $UTENTE -c "kate"
-wget http://dl.dropbox.com/u/21763079/choqokrc
+su $UTENTE -c "choqok"
+wget -b http://dl.dropbox.com/u/21763079/choqokrc
 mv ./choqokrc /home/mte90/.kde/share/config/choqokrc
 
 echo "
 Imposto Yakuake all'avvio"
-wget http://dl.dropbox.com/u/21763079/Yakuake.desktop
+wget -b http://dl.dropbox.com/u/21763079/Yakuake.desktop
 cp ./Yakuake.desktop /home/mte90/.config/autostart/Yakuake.desktop
 
 echo "
@@ -468,6 +445,7 @@ user_pref("http.proxy.pipelining", true);
 user_pref("network.http.pipelining", true);
 user_pref("network.http.pipelining.maxrequests", 10);
 user_pref("content.switch.threshold", 250000);
+user_pref("middlemouse.contentLoadURL", false);
 ' > ./user.js
 
 while true; do
@@ -501,7 +479,7 @@ rm -rfvI ~/.local/share/Trash/info/'
 chmod +x /home/$UTENTE/trash.sh
 
 echo "Recoveryfont.sh - Ricarica la conf dei font di kde, utile dopo aver aggiornato i font succede che si sputanna tutto"
-cp /home/mte90/.kde/share/config/kdeglobals /home/mte90/.kde/share/config/_kdeglobals
+cp /home/$UTENTE/.kde/share/config/kdeglobals /home/$UTENTE/.kde/share/config/_kdeglobals
 echo "
 #!/bin/bash
 cd /home/$UTENTE/.kde/share/config/
@@ -523,3 +501,74 @@ rm ./README
 > /home/$UTENTE/gmic.sh
 
 chmod +x /home/$UTENTE/gmic.sh
+
+while true; do
+    read -n 1 -p "Vuoi proseguire?" sn
+    case $sn in
+        [Ss]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Si o no.";;
+    esac
+done
+
+echo "Gimp - Palette, Patterns, Brushes, Gradients"
+cd /home/$UTENTE/Desktop/install_
+mkdir ./gimp
+wget -b http://gimp-tutorials.net/files/130-UltimateWeb2-0-Gradients-for-Gimp.zip
+unzip ./130-UltimateWeb2-0-Gradients-for-Gimp.zip
+wget -b http://www.deviantart.com/download/244975649/30_gimp_gradients_by_frostbo-d41uof5.zip
+unzip ./30_gimp_gradients_by_frostbo-d41uof5.zip
+mv ./Gimp\ Gradient\ pack\ 30\ by\ frost/* /usr/share/gimp/2.0/gradients/
+wget -b http://gps-gimp-paint-studio.googlecode.com/files/GPS%201_5_final%20release.zip
+unzip ./GPS\ 1_5_final\ release.zip
+cd ./brushes
+wget -b http://www.deviantart.com/download/124578466/3D_Gimp_Brush_set_by_DimondDoves.zip
+unzip 3D_Gimp_Brush_set_by_DimondDoves.zip
+rm wget_log
+cd ../
+mv gradients/* /usr/share/gimp/2.0/gradients/
+mv brushes/* /usr/share/gimp/2.0/brushes/
+mv patterns/* /usr/share/gimp/2.0/patterns/
+mv palettes/* /usr/share/gimp/2.0/palettes/
+mv tool-options/* /home/$UTENTE/.gimp-2.8/tool-options/
+
+echo "Gimp - Scripts"
+mkdir ./scripts
+cd ./scripts
+wget -b http://www.deviantart.com/download/30316704/GLASS_SCRIPT_FU_runs_on_2_4_by_kward1979uk.scm
+wget -b http://shallowsky.com/software/pandora/pandora-combine-0.9.3.scm
+wget -b http://registry.gimp.org/files/layers-slices_1.scm
+wget -b http://www.deviantart.com/download/74282791/Retro_Background_Script_by_fence_post.scm
+wget -b http://www.deviantart.com/download/77556760/GIMP_2_6_and_2_4_Satin_Script_by_fence_post.zip
+wget -b http://www.deviantart.com/download/123319582/Electronic_GIMP_Script_by_mikethedj4.scm
+wget -b http://www.deviantart.com/download/71646868/Add_Border_Script_by_Insanity_Prevails.scm
+wget -b http://www.deviantart.com/download/273071819/gimp_script___fit_all_layers_to_image_size_by_elheartista-d4ikvkb.zip
+unzip ./GIMP_2_6_and_2_4_Satin_Script_by_fence_post.zip
+unzip ./gimp_script___fit_all_layers_to_image_size_by_elheartista-d4ikvkb.zip
+rm ./GIMP_2_6_and_2_4_Satin_Script_by_fence_post.zip
+rm ./gimp_script___fit_all_layers_to_image_size_by_elheartista-d4ikvkb.zip
+rm ./wget_log
+cd ../
+mv scripts/* /usr/share/gimp/2.0/scripts
+
+echo "Gimp - Plugin"
+wget -b http://files.myopera.com/area42/files/cssdev.py
+chmod +x ./cssdev.py
+mv ./cssdev.py /usr/lib/gimp/2.0/plug-ins/
+wget -b http://registry.gimp.org/files/gimp-plugin-toy-1.0.4.tar.gz
+tar zxvf ./gimp-plugin-toy-1.0.4.tar.gz
+cd ./gimp-plugin-toy-1.0.4
+./configure
+make && make install
+cd ../
+wget -b -O resynth.tar.gz https://github.com/bootchk/resynthesizer/tarball/master
+tar zxvf ./resynth.tar.gz
+cd `tar tzf resynth.tar.gz | head -1`
+./autogen.sh
+make && make install
+
+echo "Rimozione file scaricati"
+cd /home/$UTENTE/Desktop/
+rm -r ./install_
+
+echo "Installazione finita"
