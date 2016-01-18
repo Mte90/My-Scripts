3f1069bbcca18d97486c9d80727c456290a77b7a
Daniele Scasciafratte <mte90net@gmail.com>
2016-01-19 00:13:40 +0100

    append new line

--- coffeescript.sh
+++ coffeescript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Require apt-get install -y inotify-tools
-while inotifywait -rq assets/coffee/ -e create -e move -e modify -e delete; do  
+while inotifywait -rq -e create -e move -e modify -e delete assets/coffee/; do  
     echo "Compiled"
-    cat assets/coffee/*.coffee | coffee -bcs > assets/js/script.js && tmp=$(cat assets/js/script.js); echo -e "jQuery.ready(function(){\n$tmp\n});" > assets/js/script.js
+    sed -e '$s/$/\n/' -s assets/coffee/*.coffee | coffee -bcs > assets/js/script.js 
+    tmp=$(cat assets/js/script.js) 
+    echo -e "jQuery.ready(function(){\n$tmp\n});" > assets/js/script.js
 done
