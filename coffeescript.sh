6dec0a7d243fee0b554140870a16577e6b8908e2
Daniele Scasciafratte <mte90net@gmail.com>
2016-01-19 00:51:48 +0100

    coffeescript with jquery wrapper

--- coffeescript.sh
+++ coffeescript.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# Compile a folder with multiple coffeescript files to a single javascript with sourcemap
+# Include a diff percentage
 #Require apt-get install -y inotify-tools
-while inotifywait -rq -e create -e move -e modify -e delete assets/coffee/; do  
-    echo "Compiled"
-    sed -e '$s/$/\n/' -s assets/coffee/*.coffee | coffee -bcs > assets/js/script.js 
+while inotifywait -rq -e create -e modify,attrib,move,create,delete assets/coffee/*.coffee; do 
+    SIZE1=$(stat -c "%s" "assets/js/script.js")
+    sed -e '$s/$/\n/' -s assets/coffee/*.coffee > assets/js/script.coffee
+    coffee -bcm -o assets/js assets/js/script.coffee
+    rm assets/js/script.coffee
     tmp=$(cat assets/js/script.js) 
-    echo -e "jQuery.ready(function(){\n$tmp\n});" > assets/js/script.js
+    echo -e "jQuery.ready(function(){\n$tmp\n});\n" > assets/js/script.js
+    SIZE2=$(stat -c "%s" "assets/js/script.js")
+    PERC=$(bc <<< "scale=2; ($SIZE2 - $SIZE1)/$SIZE1 * 100")
+    echo "  Compiled Diff: $PERC%"
 done
