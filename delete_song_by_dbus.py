02427f256437867947bfc9eae23a8c4955107828
Daniele Scasciafratte <mte90net@gmail.com>
2017-02-08 16:26:56 +0100

    other typos

--- delete_song_by_dbus.py
+++ delete_song_by_dbus.py
@@ -10,6 +10,6 @@ player = session_bus.get_object('org.mpris.MediaPlayer2.vlc', '/org/mpris/MediaP
 interface = dbus.Interface(player, dbus_interface='org.mpris.MediaPlayer2.Player')
 metadata = player.Get('org.mpris.MediaPlayer2.Player', 'Metadata', dbus_interface='org.freedesktop.DBus.Properties')
 
-os.remove(metadata['xesam:url'].replace('%23','#').replace('%28','(').replace('%29',')').replace('%26','&').replace('%20',' ').replace('file://',''))
+os.remove(metadata['xesam:url'].replace('%23','#').replace('%28','(').replace('%29',')').replace('%27',"'").replace('%26','&').replace('%20',' ').replace('file://',''))
 
 interface.Next()
