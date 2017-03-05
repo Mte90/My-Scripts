0f2693e52aeefd3fe2b2132a72f4b769c4e69a18
Daniele Scasciafratte <mte90net@gmail.com>
2017-03-06 00:11:59 +0100

    new scripts

---
+++ git-shrink.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git reflog expire --expire=now --all
+git -c gc.reflogExpire=0 -c gc.reflogExpireUnreachable=0 -c gc.rerereresolved=0 -c gc.rerereunresolved=0 -c gc.pruneExpire=now gc "$@"
