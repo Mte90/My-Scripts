6918428bca5895bfedd7ab1c306681cc0614f396
Daniele Scasciafratte <mte90net@gmail.com>
2017-11-30 18:37:01 +0100

    improved download

--- misc/download_raiplay.py
+++ misc/download_raiplay.py
@@ -15,8 +15,19 @@ tree = lxml.html.fromstring(html)
 sel = CSSSelector('a.video, .puntateItem a')
 results = sel(tree)
 
+loop = 0
+total = 20
+if 3 in sys.argv:
+    total = sys.argv[3]
+
 for link in results:
     link = 'http://www.raiplay.it' + link.get('href')
 
-    with youtube_dl.YoutubeDL({'nooverwrites': True, 'forcetitle': True}) as ydl:
+    if loop == total:
+        break
+    loop += 1
+
+    with youtube_dl.YoutubeDL({'nooverwrites': True, 'forcetitle': True, 'quiet': True}) as ydl:
         ydl.download([link])
+
+print("\nDownloaded " + str(loop) + " files")
