4f65b5d254ad80eb8da4e22d699371bd7e191c9f
Daniele Scasciafratte <mte90net@gmail.com>
2018-05-07 19:59:11 +0200

    few updates

--- misc/udoo-mount.sh
+++ misc/udoo-mount.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+shopt -s extglob
 
 if (exec 3<>/dev/tcp/192.168.1.10/22) 2> /dev/null; then
     sshfs -o password_stdin root@192.168.1.10:/var/www /mnt/dandoo <<< "dandoo"
-    cd /mnt/dandoo
     echo "Mounted"
+    # support for Exakat
+    sshpass -p "dandoo" rsync -avz --exclude '*vendor*' --exclude '.padawan' /var/www/VVV/www/glossary/htdocs/wp-content/plugins/glossary/ root@192.168.1.10:/var/exakat/projects/glossary/code/
 fi
