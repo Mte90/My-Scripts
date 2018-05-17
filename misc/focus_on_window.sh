bd6e3a7da9c4a3108dbc7571540378df06dc0c77
Daniele Scasciafratte <mte90net@gmail.com>
2018-05-17 16:28:58 +0200

    updated

--- misc/focus_on_window.sh
+++ misc/focus_on_window.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" -eq "1" ]; then
-    xdotool mousemove 900 5 click 1
+    xdotool mousemove 900 5
 elif [ "$1" -eq "2" ]; then
-    xdotool mousemove 2560 40 click 1
+    xdotool mousemove 2560 40
 elif [ "$1" -eq "3" ]; then
-    xdotool mousemove 4000 5 click 1
+    xdotool mousemove 4000 5
 fi
