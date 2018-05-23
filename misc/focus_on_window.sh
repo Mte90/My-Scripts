b93c770febd2f4d56d0cb4c9b06b928efa14f5bd
Daniele Scasciafratte <mte90net@gmail.com>
2018-05-24 01:16:08 +0200

    improve behaviour on third monitor

--- misc/focus_on_window.sh
+++ misc/focus_on_window.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+eval $(xdotool getmouselocation --shell)
+
 if [ "$1" -eq "1" ]; then
-    xdotool mousemove 900 5
+    xdotool mousemove 900 $Y
 elif [ "$1" -eq "2" ]; then
-    xdotool mousemove 2560 40
+    xdotool mousemove 2560 $Y
 elif [ "$1" -eq "3" ]; then
-    xdotool mousemove 4000 5
+    if [ $Y -gt 700 ]; then
+        Y=700
+    fi
+    xdotool mousemove 4000 $Y
 fi
