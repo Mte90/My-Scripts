96a51f73f4359e4a8ff8cf8229c8d5c8535ab123
Mte90 <mte90net@gmail.com>
2018-12-17 21:33:00 +0100

    put in queue on amule and torrent

--- misc/download_ed2k_torrent.sh
+++ misc/download_ed2k_torrent.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+mkdir /tmp/download
 cd /tmp/download
 
-ed2k -e *.emulecollection
+# First download the assets to elaborate
+wget [your-url] /tmp/download
+unzip download
 
-transmission-remote -a *.torrent
+cd ./amule
+
+# Put in queue
+if ls ./*.emulecollection 1> /dev/null 2>&1; then
+    ed2k -e *.emulecollection
+fi
+
+if ls ./*.torrent 1> /dev/null 2>&1; then
+    yes | transmission-remote -a *.torrent --auth transmission:torrent
+fi
