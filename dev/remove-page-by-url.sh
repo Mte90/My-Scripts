d409e12e3e13db136bea91d9a8be4e325563e460
Daniele Scasciafratte <mte90net@gmail.com>
2019-01-02 18:25:13 +0100

    remove page by url

---
+++ dev/remove-page-by-url.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install:
+#     wp package install codeAtcode/wp-cli-getbyurl
+# How to use the script:
+#     cat list.txt | xargs -n1 remove-page-by-url.sh
+
+url="https://domain/$1"
+out=`wp get-by-url $url`
+command=`cut -d'|' -f1 <<< $out`
+id=`cut -d'|' -f2 <<< $out`
+taxonomy=`cut -d'|' -f3 <<< $out`
+if [ $taxonomy == 'post' ];
+    wp $command delete $id
+else
+    wp term delete $taxonomy $id
+fi
