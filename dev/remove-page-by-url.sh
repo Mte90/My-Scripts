754239a76dbbd4ece4503597f0560e488c8bc2cf
Daniele Scasciafratte <mte90net@gmail.com>
2019-01-10 12:51:07 +0100

    optimized script

--- dev/remove-page-by-url.sh
+++ dev/remove-page-by-url.sh
@@ -6,12 +6,12 @@
 #     cat list.txt | xargs -n1 remove-page-by-url.sh
 
 url="https://domain/$1"
-out=`wp get-by-url $url`
+out=`wp get-by-url $url --skip-plugins --skip-themes`
 command=`cut -d'|' -f1 <<< $out`
 id=`cut -d'|' -f2 <<< $out`
 taxonomy=`cut -d'|' -f3 <<< $out`
 if [ $taxonomy == 'post' ];
-    wp $command delete $id
+    wp $command delete $id --skip-plugins --skip-themes
 else
-    wp term delete $taxonomy $id
+    wp term delete $taxonomy $id --skip-plugins --skip-themes
 fi
