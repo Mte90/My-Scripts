9670256f3e101e39abb846cb95f6271cfe0b8f87
Daniele Scasciafratte <mte90net@gmail.com>
2019-01-10 18:27:15 +0100

    fixed script

--- dev/remove-page-by-url.sh
+++ dev/remove-page-by-url.sh
@@ -5,13 +5,14 @@
 # How to use the script:
 #     cat list.txt | xargs -n1 remove-page-by-url.sh
 
-url="https://domain/$1"
-out=`wp get-by-url $url --skip-plugins --skip-themes`
-command=`cut -d'|' -f1 <<< $out`
-id=`cut -d'|' -f2 <<< $out`
-taxonomy=`cut -d'|' -f3 <<< $out`
-if [ $taxonomy == 'post' ];
-    wp $command delete $id --skip-plugins --skip-themes
-else
-    wp term delete $taxonomy $id --skip-plugins --skip-themes
+out=$(wp get-by-url "$1" --skip-plugins --skip-themes)
+if [[ -n $out ]]; then
+    command=$(cut -d'|' -f1 <<< "$out" | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
+    id=$(cut -d'|' -f2 <<< "$out" | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
+    taxonomy=$(cut -d'|' -f3 <<< "$out" | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
+    if [[ "$taxonomy" == 'post' ]]; then
+        wp "$command" delete "$id" --skip-plugins --skip-themes
+    else
+        wp "$command" delete "$taxonomy" "$id" --skip-plugins --skip-themes
+    fi
 fi
