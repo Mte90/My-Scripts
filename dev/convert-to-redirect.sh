9d564baec51bc59cd6152e3f7445875a876f1074
Daniele Scasciafratte <mte90net@gmail.com>
2019-08-06 15:37:27 +0200

    fix htaccess rules

--- dev/convert-to-redirect.sh
+++ dev/convert-to-redirect.sh
@@ -3,9 +3,9 @@
 # How to use the script:
 #     cat list.txt | xargs -n1 convert-to-redirect.sh
 
-strip='https\:\/\/domain.tld\/'
+strip='https\:\/\/domain.tld'
 redirect='https\:\/\/domain2.tld\/'
-redirectmatch_htaccess='RedirectMatch 301 \^'
-redirect_htaccess=$(echo "$ $redirect" | tr '\n' ' ')
+redirectmatch_htaccess='Redirect 301 '
+redirect_htaccess=$(echo "/ $redirect" | tr '\n' ' ')
 
 sed "s/$strip/$redirectmatch_htaccess/g;s/.$/$redirect_htaccess/;" list.txt > htaccess_redirect
