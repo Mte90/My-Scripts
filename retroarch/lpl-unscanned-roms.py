b04284134fdc4fdf1c231cba00ead1bd7104b606
Daniele Scasciafratte <mte90net@gmail.com>
2019-09-04 17:52:44 +0200

    detect what roms are not scanned by retroarch

---
+++ retroarch/lpl-unscanned-roms.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import os
+
+parser = argparse.ArgumentParser(description='Scan a thumbnail from a .lpl')
+parser.add_argument('--playlist', help='Playlist file', nargs='?', action='store', const='', default='')
+parser.add_argument('--remap', help='Path of the USB stick in the computer where is running the script', nargs='?', action='store', const='', default='/media/')
+args = parser.parse_args()
+
+if not os.path.exists(args.playlist):
+    print(args.playlist + ' doesn\'t exists.')
+    exit
+
+
+def get_console_name(console):
+    console = os.path.splitext(os.path.basename(console))[0]
+    if 'MAME' in console:
+        return 'MAME'
+    return console
+
+
+console = get_console_name(args.playlist)
+roms = {}
+
+print('Scanning ' + console + ' thumbnails')
+with open(args.playlist, "r") as read_file:
+    data = json.load(read_file)
+    if len(data['items']) > 0:
+        for item in data['items']:
+            roms[item['path'].replace('/media/', args.remap)] = True
+            rom_folder = os.path.dirname(item['path'].replace('/media/', args.remap))
+    else:
+        print('Empty playlist.')
+        exit
+
+for r, d, f in os.walk(rom_folder):
+    for file in f:
+        rom_name = os.path.basename(file)
+        remap_rom = (r + '/' + file)
+        if remap_rom not in roms:
+            print(' ROM not reconized: ' + rom_name)
