bc0b6402211a135e09462ea6f4da516db78bd5ea
Daniele Scasciafratte <mte90net@gmail.com>
2019-11-08 11:41:18 +0100

    updates

--- misc/download_raiplay.py
+++ misc/download_raiplay.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# Dependence to install: apt install python3-cssselect python3-lxml youtube-dl
+
 # First parameter is the page like: http://www.raiplay.it/programmi/cameracafe/
 # Second parameter where download the file
 # If the file is already available is not downloaded
@@ -7,27 +9,32 @@
 import lxml.html, sys, os
 from lxml.cssselect import CSSSelector
 from urllib.request import urlopen
+import requests
 import youtube_dl
 
 os.chdir(sys.argv[2])
 html = urlopen(sys.argv[1]).read().decode('utf-8')
 tree = lxml.html.fromstring(html)
-sel = CSSSelector('a.video, .puntateItem a')
+sel = CSSSelector('rai-episodes')
 results = sel(tree)
 
+json_url = 'https://www.raiplay.it/' + results[0].get('base_path')
+json_url += '/' + results[0].get('block')
+json_url += '/' + results[0].get('set') + '/episodes.json'
+
+# Making the http request
+r = requests.get(json_url)
+
+# Transforming the data returned into JSON format
+data = r.json()
+
 loop = 0
-total = 20
-if 3 in sys.argv:
-    total = sys.argv[3]
 
-for link in results:
-    link = 'http://www.raiplay.it' + link.get('href')
-
-    if loop == total:
-        break
-    loop += 1
-
-    with youtube_dl.YoutubeDL({'nooverwrites': True, 'forcetitle': True, 'quiet': True}) as ydl:
-        ydl.download([link])
+for season in data['seasons']:
+    for episode in season['episodes'][0]['cards']:
+        video_page = 'https://www.raiplay.it/video/' + episode['weblink']
+        with youtube_dl.YoutubeDL({'nooverwrites': True, 'forcetitle': True, 'quiet': True}) as ydl:
+            ydl.download([video_page])
+            loop += 1
 
 print("\nDownloaded " + str(loop) + " files")
