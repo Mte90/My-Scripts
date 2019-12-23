cad5f4ef6263630b668147c244443d51b85f524d
Daniele Scasciafratte <mte90net@gmail.com>
2019-12-23 22:11:11 +0100

    support for an unique mame playlist

--- retroarch/lpl-unscanned-roms.py
+++ retroarch/lpl-unscanned-roms.py
@@ -3,6 +3,7 @@ import argparse
 import json
 import os
 import sys
+import glob
 
 parser = argparse.ArgumentParser(description='Scan a thumbnail from a .lpl')
 parser.add_argument('--playlist', help='Playlist file', nargs='?', action='store', const='', default='')
@@ -21,13 +22,28 @@ def get_console_name(console):
     return console
 
 
+def merge_mame_playlist(playlist):
+    result = [] 
+    path = os.path.dirname(os.path.abspath(playlist))
+    for f in glob.glob(path + "/MAME*.lpl"):
+        with open(f, "rb") as infile: 
+            only_games = json.load(infile)
+            result = only_games['items'] + result
+
+    with open("/tmp/merged_file.json", "w") as outfile: 
+        json.dump({ 'items': result}, outfile) 
+    
+    return "/tmp/merged_file.json"
+
 console = get_console_name(args.playlist)
+playlist = args.playlist
+if console is 'MAME':
+    playlist = merge_mame_playlist(args.playlist)
 roms = {}
 
 print('Scanning ' + console + ' roms')
-with open(args.playlist, "r") as read_file:
+with open(playlist, "r") as read_file:
     data = json.load(read_file)
-    print(str(len(data['items'])) + ' roms in the playlist')
     if len(data['items']) > 0:
         for item in data['items']:
             path = item['path'].replace('/media/', args.remap).split('#')
@@ -36,12 +52,16 @@ with open(args.playlist, "r") as read_file:
     else:
         print('Empty playlist.')
         sys.exit()
+    
+    print(str(len(roms)) + ' roms in the playlist')
 
 print('Local folder ' + rom_folder)
 
 for r, d, f in os.walk(rom_folder):
-    for file in f:
-        rom_name = os.path.basename(file)
-        remap_rom = (r + '/' + file)
+    for rom in f:
+        rom_name = os.path.basename(rom)
+        remap_rom = (r + '/' + rom)
         if remap_rom not in roms:
             print(' ROM not reconized: ' + rom_name)
+
+print('Script finished ')
