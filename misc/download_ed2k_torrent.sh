9fbdef037baa409a33bde6be43c325f0f729ef74
Daniele Scasciafratte <mte90net@gmail.com>
2020-03-30 18:12:06 +0200

    last updates and fixes

--- misc/download_ed2k_torrent.sh
+++ misc/download_ed2k_torrent.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir /tmp/download
+rm -fr /tmp/download
+mkdir -p /tmp/download
 cd /tmp/download
 
 # First download the assets to elaborate
@@ -11,11 +12,21 @@ cd ./amule
 
 # Put in queue
 if ls ./*.emulecollection 1> /dev/null 2>&1; then
+    awk 'NF' *.emulecollection
     ed2k -e *.emulecollection
+fi
+if ls ./*.amulecollection 1> /dev/null 2>&1; then
+    awk 'NF' *.amulecollection
+    ed2k -e *.amulecollection
 fi
 
 if ls ./*.torrent 1> /dev/null 2>&1; then
     for i in *.torrent; do
-        yes | transmission-remote -a *.torrent --auth transmission:torrent
+        yes | transmission-remote -a "$i" --auth transmission:transmission
     done
 fi
+
+for filename in ./*; do
+    [ -e "$filename" ] || continue
+    curl --header "Authorization: Basic [atokenstring]" -X DELETE "https://[your-url]/public.php/webdav/$filename"
+done
