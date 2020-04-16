900eb78e6f9619f62356e86d4009aebde5d922ce
Daniele Scasciafratte <mte90net@gmail.com>
2020-04-16 20:24:35 +0200

    force tool

--- misc/edit_deb_control.sh
+++ misc/edit_deb_control.sh
@@ -32,7 +32,7 @@ fi
 CONTROL="$TMPDIR"/DEBIAN/control
 
 MOD=`stat -c "%y" "$CONTROL"`
-$EDITOR "$CONTROL"
+vim.tiny "$CONTROL"
 
 if [[ "$MOD" == `stat -c "%y" "$CONTROL"` ]]; then
   echo Not modfied.
