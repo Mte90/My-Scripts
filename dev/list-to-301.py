b916163a53a8d9241276c8eda3cf526f2cc46d05
Daniele Scasciafratte <mte90net@gmail.com>
2020-12-10 19:26:59 +0100

    fix(htaccess): better redirection rule

--- dev/list-to-301.py
+++ dev/list-to-301.py
@@ -2,9 +2,12 @@
 import csv
 import sys
 import os
+from urllib.parse import urlsplit  # Python 3
 
 if os.path.isfile(sys.argv[1]):
     with open(sys.argv[1], newline='') as csvfile:
          url = csv.reader(csvfile, delimiter=',', quotechar='|')
          for row in url:
-             print('Redirect 301 ' + row[0].replace('"', '') + ' https://' + sys.argv[2])
+             old = urlsplit(row[0].replace('"', ''))
+             new = urlsplit(row[1].replace('"', ''))
+             print('RewriteRule ^' + old.path[1:] + '$ ' + new.path + ' [R=301,L]')
