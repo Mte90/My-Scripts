a0d2a0d645fc4008e83a3b68892941c9eb225864
Daniele Scasciafratte <mte90net@gmail.com>
2021-10-02 22:33:08 +0200

    feat(files): better organization

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Why
 My bash/python scripts that I've done (or found) to improve my UX in my debian sid box.  
-For my dotfiles check https://github.com/mte90/dotfiles
-
-# Others Resources/Tools by me
-
-To a list of my own tools I invite you to check my repositories but also the dotfiles repo that is more updated about that list.
+For my dotfiles (and tools) check https://github.com/mte90/dotfiles
--- misc/udoo-mount.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-shopt -s extglob
-
-if (exec 3<>/dev/tcp/192.168.1.10/22) 2> /dev/null; then
-    sshfs -o password_stdin root@192.168.1.10:/var/www /mnt/dandoo <<< "dandoo"
-    echo "Mounted"
-    # support for Exakat
-    sshpass -p "dandoo" rsync -avz --exclude '*vendor*' --exclude '.padawan' /var/www/VVV/www/glossary/htdocs/wp-content/plugins/glossary/ root@192.168.1.10:/var/exakat/projects/glossary/code/
-fi
