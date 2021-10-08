6b822a59c5ac461c301300645ed16ff32718bc62
Daniele Scasciafratte <mte90net@gmail.com>
2021-10-08 14:51:30 +0200

    feat(vlc): mover updated

--- misc/move_song_up_by_dbus.py
+++ misc/move_song_up_by_dbus.py
@@ -5,6 +5,9 @@
 import dbus, os
 from urllib.parse import unquote
 
+def has_numbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
 session_bus = dbus.SessionBus()
 
 player = session_bus.get_object('org.mpris.MediaPlayer2.vlc', '/org/mpris/MediaPlayer2')
@@ -13,9 +16,15 @@ metadata = player.Get('org.mpris.MediaPlayer2.Player', 'Metadata', dbus_interfac
 
 path  = unquote(metadata['xesam:url']).replace('file://','')
 
-if ".mp4" not in path or ".avi" not in path:
+if ".mp3" in path or ".ogg" in path or ".flac" in path:
     folder = os.path.abspath(os.path.join(path, os.pardir)) + '/'
     folder = os.path.abspath(os.path.join(folder, os.pardir)) + '/'
-    os.rename(path, folder + os.path.basename(path))
+    first_parent = folder + os.path.basename(path)
+    os.rename(path, first_parent)
+    # If is an album folder of a discography move up * 2
+    if has_numbers(os.path.basename(os.path.dirname(path))[0:4]):
+        folder = os.path.abspath(os.path.join(folder, os.pardir)) + '/'
+        os.rename(first_parent, folder + os.path.basename(path))
+
 
 interface.Next()
--- misc/scarica.sh
+++ misc/scarica.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ago=$(date +"%Y%m%d" -d "last-monday - 2 week")
 cd /media/disk3part1/Musica/Da_Vedere/scaricati/
-youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/FunkyyPanda --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --dateafter $ago
-youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/NewRetroWave --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --dateafter $ago
-youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/MonstercatMedia --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --dateafter $ago
-youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/UCLFlh_qSWmdPkMLaI2YUxTg/ --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --dateafter $ago # Electro swing elite
-youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCtIOyeupgM3jRAn74Q1RNeQ/ --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --dateafter $ago # Stefano di carlo
-youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCr_D8RsfDhZ1CVgD7l5ByoQ --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --dateafter $ago # immortal swing
+timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/FunkyyPanda/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600"
+timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/NewRetroWave/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600"
+timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/MonstercatMedia/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600"
+timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/UCLFlh_qSWmdPkMLaI2YUxTg/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600" # Electro swing elite
+timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCtIOyeupgM3jRAn74Q1RNeQ/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # Stefano di carlo
+timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCr_D8RsfDhZ1CVgD7l5ByoQ/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # immortal swing
