ddfff71a8fa24923e8c702eca5ace0d346a9e39e
Daniele Scasciafratte <mte90net@gmail.com>
2022-03-04 17:53:46 +0100

    style(scripts): updated

--- dev/wp-verify.sh
+++ dev/wp-verify.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 wp option update admin_email_lifespan $(date -d "+6 month" +'%s')
--- misc/change-midi-key-c-major.py
+++ misc/change-midi-key-c-major.py
@@ -13,8 +13,8 @@ import os
 import music21
 
 # major conversions
-majors = dict([("A-", 4),("A", 3),("B-", 2),("B", 1),("C", 0),("D-", -1),("D", -2),("E-", -3),("E", -4),("F", -5),("G-", 6),("G", 5)])
-minors = dict([("A-", 1),("A", 0),("B-", -1),("B", -2),("C", -3),("D-", -4),("D", -5),("E-", 6),("E", 5),("F", 4),("G-", 3),("G", 2)])
+majors = dict([("A-", 4), ("A", 3), ("B-", 2), ("B", 1), ("C", 0), ("D-", -1), ("D", -2), ("E-", -3), ("E", -4), ("F", -5), ("G-", 6), ("G", 5)])
+minors = dict([("A-", 1), ("A", 0), ("B-", -1), ("B", -2), ("C", -3), ("D-", -4), ("D", -5), ("E-", 6), ("E", 5), ("F", 4), ("G-", 3), ("G", 2)])
 
 os.chdir("./")
 for file in glob.glob("*.mid"):
@@ -27,7 +27,7 @@ for file in glob.glob("*.mid"):
 
     elif key.mode == "minor":
         halfSteps = minors[key.tonic.name]
-    
+
     newscore = score.transpose(halfSteps)
     key = newscore.analyze('key')
-    newscore.write('midi',file)
+    newscore.write('midi', file)
