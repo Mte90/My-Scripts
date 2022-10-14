86e1d9e93151a329a768dc5fc93e08fe7a68db2e
Daniele Scasciafratte <mte90net@gmail.com>
2022-10-14 14:19:54 +0200

    feat(scarica): rename files

--- misc/scarica.sh
+++ misc/scarica.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 ago=$(date +"%Y%m%d" -d "last-monday - 2 week")
-cd /media/disk3part1/Musica/Da_Vedere/scaricati/
-timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/FunkyyPanda/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600"
-timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/NewRetroWave/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600"
-timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/MonstercatMedia/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600"
-timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/UCLFlh_qSWmdPkMLaI2YUxTg/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600" # Electro swing elite
-timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCtIOyeupgM3jRAn74Q1RNeQ/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # Stefano di carlo
-timeout 2m youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCr_D8RsfDhZ1CVgD7l5ByoQ/featured --max-download 3 -o "%(title)s-%(id)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # immortal swing
+cd /tmp/
+youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/FunkyyPanda/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600"
+youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/NewRetroWave/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600"
+youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/MonstercatMedia/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600"
+youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user/UCLFlh_qSWmdPkMLaI2YUxTg/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600" # Electro swing elite
+youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCtIOyeupgM3jRAn74Q1RNeQ/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # Stefano di carlo
+youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCr_D8RsfDhZ1CVgD7l5ByoQ/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # immortal swing
+
+rename 's/\#complextro//g' ./*
+rename 's/\#deephouse//g' ./*
+rename 's/\#discohouse//g' ./*
+rename 's/\#edm//g' ./*
+rename 's/\#electrohouse//g' ./*
+rename 's/\#electronic//g' ./*
+rename 's/\#electroswing//g' ./*
+rename 's/\#frenchtouch//g' ./*
+rename 's/\#glitchhop//g' ./*
+rename 's/\#indie//g' ./*
+rename 's/\#italodisco//g' ./*
+rename 's/\#jpop//g' ./*
+rename 's/\#retrowave//g' ./*
+rename 's/\#synthwave//g' ./*
+rename 's/\#nudisco//g' ./*
+
+rename 's/ _ //g' ./*
