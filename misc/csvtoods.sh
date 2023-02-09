b932328fe936fc313e8a67e2e1814040c098e125
Daniele Scasciafratte <mte90net@gmail.com>
2023-02-09 18:54:14 +0100

    feat(scripts): more!

---
+++ misc/csvtoods.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# richiede gnumeric
+
+ssconvert --merge-to=database.ods *.csv
--- misc/yakuake.sh
+++ misc/yakuake.sh
@@ -22,6 +22,8 @@ instruct "/Sessions/1 org.kde.konsole.Session.sendText \$'\n'"
 instruct "/Sessions/1 org.kde.konsole.Session.sendText $1"
 instruct "/Sessions/1 org.kde.konsole.Session.sendText \$'\n'"
 
+sleep 1
+
 instruct "/yakuake/sessions org.kde.yakuake.runCommandInTerminal 1 'vvv'"
 instruct "/yakuake/sessions org.kde.yakuake.runCommandInTerminal 2 'casa'"
 instruct "/yakuake/sessions org.kde.yakuake.runCommandInTerminal 1 'clear'"
