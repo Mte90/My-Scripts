29ef306ce480a9b31ebc04d5dc6caf0ee569dead
Daniele Scasciafratte <mte90net@gmail.com>
2023-10-13 10:56:18 +0200

    fix(youtube): clean filenames

--- misc/download_youtube_video.sh
+++ misc/download_youtube_video.sh
@@ -8,20 +8,86 @@ youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/user
 youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCtIOyeupgM3jRAn74Q1RNeQ/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # Stefano di carlo
 youtube-dl --extract-audio --audio-format=mp3 -w -c https://www.youtube.com/channel/UCr_D8RsfDhZ1CVgD7l5ByoQ/featured --max-download 3 -o "%(title)s.%(ext)s" --match-filter "duration < 600" --dateafter $ago # immortal swing
 
+rename 's/\ Visualizer\ //g' ./*
+rename 's/\#basshouse//g' ./*
 rename 's/\#complextro//g' ./*
+rename 's/\#dance//g' ./*
 rename 's/\#deephouse//g' ./*
 rename 's/\#discohouse//g' ./*
+rename 's/\#DiscoHouse//g' ./*
 rename 's/\#edm//g' ./*
 rename 's/\#electrohouse//g' ./*
 rename 's/\#electronic//g' ./*
 rename 's/\#electroswing//g' ./*
+rename 's/\#electrohouse//g' ./*
+rename 's/\#electronic//g' ./*
+rename 's/\#electroswing//g' ./*
+rename 's/\#ElectroSwing//g' ./*
 rename 's/\#frenchtouch//g' ./*
+rename 's/\#futurefunk//g' ./*
+rename 's/\#FutureFunk//g' ./*
+rename 's/\#Futurefunk//g' ./*
+rename 's/\#futurebass//g' ./*
+rename 's/\#funky//g' ./*
+rename 's/\#funk//g' ./*
 rename 's/\#glitchhop//g' ./*
+rename 's/\#indiedance//g' ./*
 rename 's/\#indie//g' ./*
 rename 's/\#italodisco//g' ./*
 rename 's/\#jpop//g' ./*
-rename 's/\#retrowave//g' ./*
-rename 's/\#synthwave//g' ./*
+rename 's/\#music//g' ./*
 rename 's/\#nudisco//g' ./*
+rename 's/\#NuDisco//g' ./*
+rename 's/\#retrowave//g' ./*
+rename 's/\#Retrowave//g' ./*
+rename 's/\#soulhouse//g' ./*
+rename 's/\#swing//g' ./*
+rename 's/\#synthwave//g' ./*
+rename 's/\#synthpop//g' ./*
+rename 's/\#techhouse//g' ./*
+rename 's/\[Dubstep\]//g' ./*
+rename 's/\[Electronic]//g' ./*
+rename 's/\[Electro\]//g' ./*
+rename 's/\[Funkyway Release\]//g' ./*
+rename 's/\[Funky way Release\]//g' ./*
+rename 's/\[House\]//g' ./*
+rename 's/\[Lyric Video\]//g' ./*
+rename 's/\[Monstercat Release\]//g' ./*
+rename 's/\[Monstercat Lyric Video\]//g' ./*
+rename 's/\[Monstercat EP Release\]//g' ./*
+rename 's/\[Monstercat Official Music Video\]//g' ./*
+rename 's/\[Monstercat Release\]//g' ./*
+rename 's/\[Official Video\]//g' ./*
+rename 's/\[Silk Music\]//g' ./*
+rename 's/\(Electro Swing\)//g' ./*
+rename 's/\(Future Swing\)//g' ./*
+rename 's/\(Jazzy Electro Swing\)//g' ./*
+rename 's/\(Official Video\)//g' ./*
+rename 's/\(Official Music Video\)//g' ./*
+rename 's/Disco Funk//g' ./*
+rename 's/Electro House//g' ./*
+rename 's/Electro Pop//g' ./*
+rename 's/ElectroPop//g' ./*
+rename 's/Electro Swing//g' ./*
+rename 's/Future Funk//g' ./*
+rename 's/Future BASS//g' ./*
+rename 's/FutureBASS//g' ./*
+rename 's/French ELECTRO//g' ./*
+rename 's/Future HOUSE//g' ./*
+rename 's/FrenchELECTRO//g' ./*
+rename 's/FutureHOUSE//g' ./*
+rename 's/Hip Hop//g' ./*
+rename 's/NuDISCO//g' ./*
+rename 's/Nu Funk//g' ./*
+rename 's/Nu Disco//g' ./*
+rename 's/Synthwave//g' ./*
 
 rename 's/ _ //g' ./*
+rename 's/ \| //g' ./*
+rename 's/｜//g' ./*
+rename 's/＂//g' ./*
+
+
+rename 's/_//g' ./*
+rename 's/  //g' ./*
+rename 's/ \.mp3/\.mp3/g' ./*
