24aae884f0b6f7319d09f98e61c2ac3b4b1a42c7
Daniele Scasciafratte <mte90net@gmail.com>
2024-01-17 12:04:58 +0100

    fix(nvim): force a socket

--- misc/nvim-qt.py
+++ misc/nvim-qt.py
@@ -7,13 +7,11 @@ if len(sys.argv) < 2:
     os.system('/usr/local/bin/nvim-qt')
     exit(1)
 
-instances = [path for path in os.listdir("/tmp") if path.startswith("nvim")]
-if len(instances) > 1:
-    print("Multiple neovim instances running, can't decide which one should be used")
-    exit(1)
-elif len(instances) == 0:
+os.environ["NVIM_LISTEN_ADDRESS"] = '/tmp/nvim-socket'
+
+if not os.path.exists(os.environ.get("NVIM_LISTEN_ADDRESS", '')):
     sys.argv.pop(0)
     os.system('/usr/local/bin/nvim-qt ' + os.path.abspath(" ".join(sys.argv)))
 else:
-    nvim = attach("socket", path=os.path.join("/tmp", instances[0], "0"))
+    nvim = attach("socket", path=os.environ.get("NVIM_LISTEN_ADDRESS", None))
     nvim.command("tabnew " + os.path.abspath(sys.argv[1]))
