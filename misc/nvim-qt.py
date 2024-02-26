06b34a6fc250f3a84ba00a62949d4121ea4630d4
Daniele Scasciafratte <mte90net@gmail.com>
2024-02-26 14:13:16 +0100

    fix(nvim): updated launcher

--- misc/nvim-qt.py
+++ misc/nvim-qt.py
@@ -3,13 +3,19 @@ import os
 import sys
 from pynvim import attach
 
-if len(sys.argv) < 2:
+
+if len(sys.argv) == 1:
     os.system('/usr/local/bin/nvim-qt')
     exit(1)
 
 os.environ["NVIM_LISTEN_ADDRESS"] = '/tmp/nvim-socket'
 
 if not os.path.exists(os.environ.get("NVIM_LISTEN_ADDRESS", '')):
+    if os.path.isdir(os.path.abspath(os.getcwd()) + '/.venv/bin/'):
+        print("Venv found")
+        activate_this_file = os.path.abspath(os.getcwd()) + "/.venv/bin/activate_this.py"
+        exec(compile(open(activate_this_file, "rb").read(), activate_this_file, 'exec'), dict(__file__=activate_this_file))
+        print("Venv executed")
     sys.argv.pop(0)
     os.system('/usr/local/bin/nvim-qt ' + os.path.abspath(" ".join(sys.argv)))
 else:
