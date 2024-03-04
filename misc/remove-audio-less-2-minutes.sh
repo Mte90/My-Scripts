b5d5fa2fa3b563d02426a773e008e1b5b182e3d4
Daniele Scasciafratte <mte90net@gmail.com>
2024-03-04 15:01:00 +0100

    fix(audio): mp3/flac detection

--- misc/remove-audio-less-2-minutes.sh
+++ misc/remove-audio-less-2-minutes.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-find "$(pwd)" -type f -print0 | xargs -0 -I {} mediainfo --Inform="General;%Duration%,%CompleteName%" {} > /tmp/list.txt
+echo "Generating file list"
+find "$1" -type f \( -iname \*.mp3 -o -iname \*.flac \) -print0 | xargs -0 -I {} mediainfo --Inform="General;%Duration%,%CompleteName%" {} > /tmp/list.txt
 
 # Duration is in milliseconds
 
@@ -8,6 +9,7 @@ while IFS=, read -r ms path
 do
     if [[ $ms -lt 120000 ]]; then
         if [[ ! -z $path ]]; then
+            echo "Removing $path"
             rm "$path"
         fi
     fi
