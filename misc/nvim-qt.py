bb3b499afda4a5f62b3135f82393b4d7946d691c
Daniele Scasciafratte <mte90net@gmail.com>
2024-03-11 12:23:18 +0100

    fix(nvim): check if socket exist

--- misc/nvim-qt.py
+++ misc/nvim-qt.py
@@ -19,5 +19,10 @@ if not os.path.exists(os.environ.get("NVIM_LISTEN_ADDRESS", '')):
     sys.argv.pop(0)
     os.system('/usr/local/bin/nvim-qt ' + os.path.abspath(" ".join(sys.argv)))
 else:
-    nvim = attach("socket", path=os.environ.get("NVIM_LISTEN_ADDRESS", None))
+    try:
+        nvim = attach("socket", path=os.environ.get("NVIM_LISTEN_ADDRESS", None))
+    except:
+        os.unlink(os.environ["NVIM_LISTEN_ADDRESS"])
+        print("Retry!")
+        sys.exit()
     nvim.command("tabnew " + os.path.abspath(sys.argv[1]))
