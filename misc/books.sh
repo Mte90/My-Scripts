7130b5228021163c2ae0c20437136a2e3a7d6b55
Daniele Scasciafratte <mte90net@gmail.com>
2024-06-10 12:25:24 +0200

    feat(misc): convert odt/csv to html

---
+++ misc/books.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Convert automatically the odt files in HTML and download the wishlist from IBS>it using the Firefox browser (that I have always open)
+# So I can later upload it on my soerver with dolphin
+
+soffice --headless --convert-to htm:HTML --outdir . libri.odt
+soffice --headless --convert-to htm:HTML --outdir . ListaVari.odt
+
+firefox-nightly https://www.ibs.it/mio-ibs/lista-desideri/manage/export-wishlist/93242281
+sleep 3
+mv /home/mte90/Desktop/Libri.csv /media/disk3part1/Backup/Fumetti/LibriNuovi.csv
+soffice --headless --convert-to htm --outdir . LibriNuovi.csv
+
+mv ./libri.htm ./libri.html
+mv ./ListaVari.htm ./ListaVari.html
+mv ./LibriNuovi.htm ./LibriNuovi.html
+
+dolphin ftp://domain/libri
