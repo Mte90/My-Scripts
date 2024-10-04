cc4cec342c11169c5f6344e0daa9f7e21aa96ab1
Daniele Scasciafratte <mte90net@gmail.com>
2024-10-04 10:33:36 +0200

    feat(lsp): updated

--- dev/lsp-installer/install.sh
+++ dev/lsp-installer/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Written in [Amber](https://amber-lang.com/)
 # version: 0.3.5-alpha
-# date: 2024-09-11 17:52:32
+# date: 2024-10-04 10:32:27
 dir_exist__0_v0() {
     local path=$1
     [ -d "${path}" ]
@@ -28,8 +28,8 @@ create_symbolic_link__5_v0() {
     local origin=$1
     local destination=$2
     file_exist__1_v0 "${origin}"
-    __AF_file_exist1_v0__37_8="$__AF_file_exist1_v0"
-    if [ "$__AF_file_exist1_v0__37_8" != 0 ]; then
+    __AF_file_exist1_v0__39_8="$__AF_file_exist1_v0"
+    if [ "$__AF_file_exist1_v0__39_8" != 0 ]; then
         ln -s "${origin}" "${destination}"
         __AS=$?
         __AF_create_symbolic_link5_v0=1
@@ -42,8 +42,8 @@ create_symbolic_link__5_v0() {
 make_executable__7_v0() {
     local path=$1
     file_exist__1_v0 "${path}"
-    __AF_file_exist1_v0__56_8="$__AF_file_exist1_v0"
-    if [ "$__AF_file_exist1_v0__56_8" != 0 ]; then
+    __AF_file_exist1_v0__59_8="$__AF_file_exist1_v0"
+    if [ "$__AF_file_exist1_v0__59_8" != 0 ]; then
         chmod +x "${path}"
         __AS=$?
         __AF_make_executable7_v0=1
@@ -78,11 +78,6 @@ is_command__79_v0() {
     __AF_is_command79_v0=1
     return 0
 }
-exit__83_v0() {
-    local code=$1
-    exit "${code}"
-    __AS=$?
-}
 is_root__84_v0() {
     __AMBER_VAL_1=$(id -u)
     __AS=$?
@@ -100,18 +95,18 @@ download__121_v0() {
     local url=$1
     local path=$2
     is_command__79_v0 "curl"
-    __AF_is_command79_v0__10_9="$__AF_is_command79_v0"
+    __AF_is_command79_v0__9_9="$__AF_is_command79_v0"
     is_command__79_v0 "wget"
-    __AF_is_command79_v0__13_9="$__AF_is_command79_v0"
+    __AF_is_command79_v0__12_9="$__AF_is_command79_v0"
     is_command__79_v0 "aria2c"
-    __AF_is_command79_v0__16_9="$__AF_is_command79_v0"
-    if [ "$__AF_is_command79_v0__10_9" != 0 ]; then
+    __AF_is_command79_v0__15_9="$__AF_is_command79_v0"
+    if [ "$__AF_is_command79_v0__9_9" != 0 ]; then
         curl -L -o "${path}" "${url}"
         __AS=$?
-    elif [ "$__AF_is_command79_v0__13_9" != 0 ]; then
+    elif [ "$__AF_is_command79_v0__12_9" != 0 ]; then
         wget "${url}" -P "${path}"
         __AS=$?
-    elif [ "$__AF_is_command79_v0__16_9" != 0 ]; then
+    elif [ "$__AF_is_command79_v0__15_9" != 0 ]; then
         aria2c "${url}" -d "${path}"
         __AS=$?
     else
@@ -125,19 +120,17 @@ is_root__84_v0
 __AF_is_root84_v0__6_8="$__AF_is_root84_v0"
 if [ $(echo '!' "$__AF_is_root84_v0__6_8" | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//') != 0 ]; then
     echo "This script requires root permissions"'!'""
-    exit__83_v0 1
-    __AF_exit83_v0__8_5="$__AF_exit83_v0"
-    echo "$__AF_exit83_v0__8_5" >/dev/null 2>&1
+    exit 1
 fi
-get_download_path__126_v0() {
+get_download_path__125_v0() {
     local repo=$1
     local position=$2
     __AMBER_VAL_2=$(curl -sL "https://api.github.com/repos/${repo}/releases" | jq -r ".[0].assets.[${position}].browser_download_url")
     __AS=$?
-    __AF_get_download_path126_v0="${__AMBER_VAL_2}"
+    __AF_get_download_path125_v0="${__AMBER_VAL_2}"
     return 0
 }
-move_to_bin__127_v0() {
+move_to_bin__126_v0() {
     local download_url=$1
     local binary=$2
     download__121_v0 "${download_url}" "${binary}" >/dev/null 2>&1
@@ -147,21 +140,17 @@ move_to_bin__127_v0() {
         __AS=$?
         if [ $__AS != 0 ]; then
             echo "Move ${binary} to /usr/local/bin failed"'!'""
-            exit__83_v0 1
-            __AF_exit83_v0__19_13="$__AF_exit83_v0"
-            echo "$__AF_exit83_v0__19_13" >/dev/null 2>&1
+            exit 1
         fi
         make_executable__7_v0 "/usr/local/bin/${binary}"
         __AF_make_executable7_v0__21_9="$__AF_make_executable7_v0"
         echo "$__AF_make_executable7_v0__21_9" >/dev/null 2>&1
     else
         echo "Download for ${binary} at ${download_url} failed"
-        exit__83_v0 1
-        __AF_exit83_v0__24_9="$__AF_exit83_v0"
-        echo "$__AF_exit83_v0__24_9" >/dev/null 2>&1
+        exit 1
     fi
 }
-download_to_bin__128_v0() {
+download_to_bin__127_v0() {
     local download_url=$1
     local binary=$2
     local packed_file=$3
@@ -186,46 +175,44 @@ download_to_bin__128_v0() {
         echo "$__AF_make_executable7_v0__39_9" >/dev/null 2>&1
     else
         echo "Download for ${binary} at ${download_url} failed"
-        exit__83_v0 1
-        __AF_exit83_v0__42_9="$__AF_exit83_v0"
-        echo "$__AF_exit83_v0__42_9" >/dev/null 2>&1
+        exit 1
     fi
 }
 cd "/tmp" || exit
 echo "Install PHPactor LSP"
-get_download_path__126_v0 "phpactor/phpactor" 0
-__AF_get_download_path126_v0__49_13="${__AF_get_download_path126_v0}"
-move_to_bin__127_v0 "${__AF_get_download_path126_v0__49_13}" "phpactor"
-__AF_move_to_bin127_v0__49_1="$__AF_move_to_bin127_v0"
-echo "$__AF_move_to_bin127_v0__49_1" >/dev/null 2>&1
+get_download_path__125_v0 "phpactor/phpactor" 0
+__AF_get_download_path125_v0__49_13="${__AF_get_download_path125_v0}"
+move_to_bin__126_v0 "${__AF_get_download_path125_v0__49_13}" "phpactor"
+__AF_move_to_bin126_v0__49_1="$__AF_move_to_bin126_v0"
+echo "$__AF_move_to_bin126_v0__49_1" >/dev/null 2>&1
 echo "Install Typos LSP"
-get_download_path__126_v0 "tekumara/typos-lsp" 6
-__AF_get_download_path126_v0__52_17="${__AF_get_download_path126_v0}"
-download_to_bin__128_v0 "${__AF_get_download_path126_v0__52_17}" "typos-lsp" "typos.tar.gz"
-__AF_download_to_bin128_v0__52_1="$__AF_download_to_bin128_v0"
-echo "$__AF_download_to_bin128_v0__52_1" >/dev/null 2>&1
+get_download_path__125_v0 "tekumara/typos-lsp" 6
+__AF_get_download_path125_v0__52_17="${__AF_get_download_path125_v0}"
+download_to_bin__127_v0 "${__AF_get_download_path125_v0__52_17}" "typos-lsp" "typos.tar.gz"
+__AF_download_to_bin127_v0__52_1="$__AF_download_to_bin127_v0"
+echo "$__AF_download_to_bin127_v0__52_1" >/dev/null 2>&1
 echo "Install Rust LSP"
-download_to_bin__128_v0 "https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz" "rust-analyzer" "rust-analyzer-x86_64-unknown-linux-gnu.gz"
-__AF_download_to_bin128_v0__55_1="$__AF_download_to_bin128_v0"
-echo "$__AF_download_to_bin128_v0__55_1" >/dev/null 2>&1
+download_to_bin__127_v0 "https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz" "rust-analyzer" "rust-analyzer-x86_64-unknown-linux-gnu.gz"
+__AF_download_to_bin127_v0__55_1="$__AF_download_to_bin127_v0"
+echo "$__AF_download_to_bin127_v0__55_1" >/dev/null 2>&1
 echo "Install GitLab CI LSP"
-get_download_path__126_v0 "alesbrelih/gitlab-ci-ls" 3
-__AF_get_download_path126_v0__58_13="${__AF_get_download_path126_v0}"
-move_to_bin__127_v0 "${__AF_get_download_path126_v0__58_13}" "gitlab-ci-ls"
-__AF_move_to_bin127_v0__58_1="$__AF_move_to_bin127_v0"
-echo "$__AF_move_to_bin127_v0__58_1" >/dev/null 2>&1
+get_download_path__125_v0 "alesbrelih/gitlab-ci-ls" 3
+__AF_get_download_path125_v0__58_13="${__AF_get_download_path125_v0}"
+move_to_bin__126_v0 "${__AF_get_download_path125_v0__58_13}" "gitlab-ci-ls"
+__AF_move_to_bin126_v0__58_1="$__AF_move_to_bin126_v0"
+echo "$__AF_move_to_bin126_v0__58_1" >/dev/null 2>&1
 echo "Install HTMX LSP"
-get_download_path__126_v0 "ThePrimeagen/htmx-lsp" 2
-__AF_get_download_path126_v0__61_13="${__AF_get_download_path126_v0}"
-move_to_bin__127_v0 "${__AF_get_download_path126_v0__61_13}" "htmx-lsp"
-__AF_move_to_bin127_v0__61_1="$__AF_move_to_bin127_v0"
-echo "$__AF_move_to_bin127_v0__61_1" >/dev/null 2>&1
+get_download_path__125_v0 "ThePrimeagen/htmx-lsp" 2
+__AF_get_download_path125_v0__61_13="${__AF_get_download_path125_v0}"
+move_to_bin__126_v0 "${__AF_get_download_path125_v0__61_13}" "htmx-lsp"
+__AF_move_to_bin126_v0__61_1="$__AF_move_to_bin126_v0"
+echo "$__AF_move_to_bin126_v0__61_1" >/dev/null 2>&1
 echo "Install Marksman LSP"
-get_download_path__126_v0 "artempyanykh/marksman" 1
-__AF_get_download_path126_v0__64_13="${__AF_get_download_path126_v0}"
-move_to_bin__127_v0 "${__AF_get_download_path126_v0__64_13}" "marksman"
-__AF_move_to_bin127_v0__64_1="$__AF_move_to_bin127_v0"
-echo "$__AF_move_to_bin127_v0__64_1" >/dev/null 2>&1
+get_download_path__125_v0 "artempyanykh/marksman" 1
+__AF_get_download_path125_v0__64_13="${__AF_get_download_path125_v0}"
+move_to_bin__126_v0 "${__AF_get_download_path125_v0__64_13}" "marksman"
+__AF_move_to_bin126_v0__64_1="$__AF_move_to_bin126_v0"
+echo "$__AF_move_to_bin126_v0__64_1" >/dev/null 2>&1
 echo "Install Lua LSP"
 dir_exist__0_v0 "/opt/lua-language-server"
 __AF_dir_exist0_v0__67_8="$__AF_dir_exist0_v0"
--- misc/books.sh
+++ misc/books.sh
@@ -9,7 +9,8 @@ soffice --headless --convert-to htm:HTML --outdir . ListaVari.odt
 firefox-nightly https://www.ibs.it/mio-ibs/lista-desideri/manage/export-wishlist/93242281
 sleep 3
 mv /home/mte90/Desktop/Libri.csv /media/disk3part1/Backup/Fumetti/LibriNuovi.csv
-soffice --headless --convert-to htm --outdir . LibriNuovi.csv
+# 59 is the ASCII value for ; (semicolon), in this way use the right separator for this file
+soffice --headless --convert-to htm --outdir . --infilter="Text - txt - csv (StarCalc)":"59,ANSI,1" LibriNuovi.csv
 
 mv ./libri.htm ./libri.html
 mv ./ListaVari.htm ./ListaVari.html
