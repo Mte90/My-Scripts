ccaaa79c82f221e8a2f12f6445a1bd682b19700e
Daniele Scasciafratte <mte90net@gmail.com>
2025-10-01 11:39:29 +0200

    feat(lsp): updated

--- dev/lsp-installer/install.sh
+++ dev/lsp-installer/install.sh
@@ -1,169 +1,194 @@
 #!/usr/bin/env bash
 # Written in [Amber](https://amber-lang.com/)
-# version: 0.4.0-alpha-43-g45cd2fa
-# date: 2025-08-04 11:23:35
+# version: 0.4.0-alpha-74-g5c640c1
 # We cannot import `bash_version` from `env.ab` because it imports `text.ab` making a circular dependency.
 # This is a workaround to avoid that issue and the import system should be improved in the future.
-text_contains__16_v0() {
+text_contains__17_v0() {
     local source=$1
     local search=$2
-    __3_result="$(if [[ "${source}" == *"${search}"* ]]; then
+    result_3="$(if [[ "${source}" == *"${search}"* ]]; then
     echo 1
   fi)"
-    __ret_text_contains16_v0="$([ "_${__3_result}" != "_1" ]; echo $?)"
+    ret_text_contains17_v0="$([ "_${result_3}" != "_1" ]; echo $?)"
     return 0
 }
 
-dir_exists__35_v0() {
+dir_exists__36_v0() {
     local path=$1
     [ -d "${path}" ]
     __status=$?
-    if [ "${__status}" != 0 ]; then
-        __ret_dir_exists35_v0=0
-        return 0
-    fi
-    __ret_dir_exists35_v0=1
+    ret_dir_exists36_v0="$(( ${__status} == 0 ))"
     return 0
 }
 
-file_exists__36_v0() {
+file_exists__37_v0() {
     local path=$1
     [ -f "${path}" ]
     __status=$?
-    if [ "${__status}" != 0 ]; then
-        __ret_file_exists36_v0=0
-        return 0
-    fi
-    __ret_file_exists36_v0=1
+    ret_file_exists37_v0="$(( ${__status} == 0 ))"
     return 0
 }
 
-symlink_create__40_v0() {
+symlink_create__41_v0() {
     local origin=$1
     local destination=$2
-    file_exists__36_v0 "${origin}"
-    __ret_file_exists36_v0__41_8="${__ret_file_exists36_v0}"
-    if [ "${__ret_file_exists36_v0__41_8}" != 0 ]; then
+    file_exists__37_v0 "${origin}"
+    ret_file_exists37_v0__37_8="${ret_file_exists37_v0}"
+    if [ "${ret_file_exists37_v0__37_8}" != 0 ]; then
         ln -s "${origin}" "${destination}"
-        __ret_symlink_create40_v0=1
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            ret_symlink_create41_v0=''
+            return "${__status}"
+        fi
+        ret_symlink_create41_v0=''
         return 0
     fi
     echo "The file ${origin} doesn't exist"'!'""
-    __ret_symlink_create40_v0=0
-    return 0
+    ret_symlink_create41_v0=''
+    return 1
 }
 
-file_chmod__44_v0() {
+file_chmod__45_v0() {
     local path=$1
     local mode=$2
-    file_exists__36_v0 "${path}"
-    __ret_file_exists36_v0__108_8="${__ret_file_exists36_v0}"
-    if [ "${__ret_file_exists36_v0__108_8}" != 0 ]; then
+    file_exists__37_v0 "${path}"
+    ret_file_exists37_v0__104_8="${ret_file_exists37_v0}"
+    if [ "${ret_file_exists37_v0__104_8}" != 0 ]; then
         chmod "${mode}" "${path}"
-        __ret_file_chmod44_v0=1
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            ret_file_chmod45_v0=''
+            return "${__status}"
+        fi
+        ret_file_chmod45_v0=''
         return 0
     fi
     echo "The file ${path} doesn't exist"'!'""
-    __ret_file_chmod44_v0=0
-    return 0
+    ret_file_chmod45_v0=''
+    return 1
 }
 
-is_command__102_v0() {
+is_command__104_v0() {
     local command=$1
     [ -x "$(command -v "${command}")" ]
     __status=$?
     if [ "${__status}" != 0 ]; then
-        __ret_is_command102_v0=0
+        ret_is_command104_v0=0
         return 0
     fi
-    __ret_is_command102_v0=1
+    ret_is_command104_v0=1
     return 0
 }
 
-is_root__107_v0() {
-    __1_command="$(id -u)"
-    if [ "$([ "_${__1_command}" != "_0" ]; echo $?)" != 0 ]; then
-        __ret_is_root107_v0=1
+is_root__109_v0() {
+    command_1="$(id -u)"
+    __status=$?
+    if [ "$([ "_${command_1}" != "_0" ]; echo $?)" != 0 ]; then
+        ret_is_root109_v0=1
         return 0
     fi
-    __ret_is_root107_v0=0
+    ret_is_root109_v0=0
     return 0
 }
 
-file_download__146_v0() {
+file_download__148_v0() {
     local url=$1
     local path=$2
-    is_command__102_v0 "curl"
-    __ret_is_command102_v0__9_9="${__ret_is_command102_v0}"
-    is_command__102_v0 "wget"
-    __ret_is_command102_v0__12_9="${__ret_is_command102_v0}"
-    is_command__102_v0 "aria2c"
-    __ret_is_command102_v0__15_9="${__ret_is_command102_v0}"
-    if [ "${__ret_is_command102_v0__9_9}" != 0 ]; then
+    is_command__104_v0 "curl"
+    ret_is_command104_v0__9_9="${ret_is_command104_v0}"
+    is_command__104_v0 "wget"
+    ret_is_command104_v0__12_9="${ret_is_command104_v0}"
+    is_command__104_v0 "aria2c"
+    ret_is_command104_v0__15_9="${ret_is_command104_v0}"
+    if [ "${ret_is_command104_v0__9_9}" != 0 ]; then
         curl -L -o "${path}" "${url}" >/dev/null 2>&1
-    elif [ "${__ret_is_command102_v0__12_9}" != 0 ]; then
+        __status=$?
+    elif [ "${ret_is_command104_v0__12_9}" != 0 ]; then
         wget "${url}" -P "${path}" >/dev/null 2>&1
-    elif [ "${__ret_is_command102_v0__15_9}" != 0 ]; then
+        __status=$?
+    elif [ "${ret_is_command104_v0__15_9}" != 0 ]; then
         aria2c "${url}" -d "${path}" >/dev/null 2>&1
+        __status=$?
     else
-        __ret_file_download146_v0=0
-        return 0
+        ret_file_download148_v0=''
+        return 1
     fi
-    __ret_file_download146_v0=1
-    return 0
 }
 
-is_root__107_v0 
-__ret_is_root107_v0__6_8="${__ret_is_root107_v0}"
-if [ "$(( ! ${__ret_is_root107_v0__6_8} ))" != 0 ]; then
+is_root__109_v0 
+ret_is_root109_v0__6_8="${ret_is_root109_v0}"
+if [ "$(( ! ${ret_is_root109_v0__6_8} ))" != 0 ]; then
     echo "This script requires root permissions"'!'""
     exit 1
 fi
-get_download_path__150_v0() {
+get_download_path__152_v0() {
     local repo=$1
     local position=$2
-    __2_command="$(curl -sL "https://api.github.com/repos/${repo}/releases" | jq -r ".[0].assets.[${position}].browser_download_url")"
+    command_2="$(curl -sL "https://api.github.com/repos/${repo}/releases" | jq -r ".[0].assets.[${position}].browser_download_url")"
     __status=$?
-    __ret_get_download_path150_v0="${__2_command}"
+    ret_get_download_path152_v0="${command_2}"
     return 0
 }
 
-move_to_bin__151_v0() {
+move_to_bin__153_v0() {
     local download_url=$1
     local binary=$2
-    file_download__146_v0 "${download_url}" "${binary}" >/dev/null 2>&1
-    __ret_file_download146_v0__16_15="${__ret_file_download146_v0}"
-    if [ "${__ret_file_download146_v0__16_15}" != 0 ]; then
+    file_download__148_v0 "${download_url}" "${binary}">/dev/null 2>&1
+    __status=$?
+    if [ "${__status}" != 0 ]; then
+        ret_move_to_bin153_v0=''
+        return "${__status}"
+    fi
+    if [ '' != 0 ]; then
         mv "${binary}" "/usr/local/bin"
         __status=$?
         if [ "${__status}" != 0 ]; then
             echo "Move ${binary} to /usr/local/bin failed"'!'""
             exit 1
         fi
-        file_chmod__44_v0 "/usr/local/bin/${binary}" "+x"
+        file_chmod__45_v0 "/usr/local/bin/${binary}" "+x"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            ret_move_to_bin153_v0=''
+            return "${__status}"
+        fi
     else
         echo "Download for ${binary} at ${download_url} failed"
         exit 1
     fi
 }
 
-download_to_bin__152_v0() {
+download_to_bin__154_v0() {
     local download_url=$1
     local binary=$2
     local packed_file=$3
-    file_download__146_v0 "${download_url}" "${packed_file}" >/dev/null 2>&1
-    __ret_file_download146_v0__29_15="${__ret_file_download146_v0}"
-    if [ "${__ret_file_download146_v0__29_15}" != 0 ]; then
-        text_contains__16_v0 "${packed_file}" "tar.gz"
-        __ret_text_contains16_v0__31_16="${__ret_text_contains16_v0}"
-        if [ "${__ret_text_contains16_v0__31_16}" != 0 ]; then
+    file_download__148_v0 "${download_url}" "${packed_file}">/dev/null 2>&1
+    __status=$?
+    if [ "${__status}" != 0 ]; then
+        ret_download_to_bin154_v0=''
+        return "${__status}"
+    fi
+    if [ '' != 0 ]; then
+        text_contains__17_v0 "${packed_file}" "tar.gz"
+        ret_text_contains17_v0__31_16="${ret_text_contains17_v0}"
+        if [ "${ret_text_contains17_v0__31_16}" != 0 ]; then
             tar -zxvf "./${packed_file}" -C ./ > /dev/null 2>&1
+            __status=$?
             mv "./${binary}" "/usr/local/bin"
+            __status=$?
         else
             gunzip -c "${packed_file}" > "/usr/local/bin/${binary}"
+            __status=$?
         fi
         rm "./${packed_file}"
-        file_chmod__44_v0 "/usr/local/bin/${binary}" "+x"
+        __status=$?
+        file_chmod__45_v0 "/usr/local/bin/${binary}" "+x"
+        __status=$?
+        if [ "${__status}" != 0 ]; then
+            ret_download_to_bin154_v0=''
+            return "${__status}"
+        fi
     else
         echo "Download for ${binary} at ${download_url} failed"
         exit 1
@@ -172,63 +197,93 @@ download_to_bin__152_v0() {
 
 cd "/tmp" || exit
 echo "Install PHPactor LSP"
-get_download_path__150_v0 "phpactor/phpactor" 0
-__ret_get_download_path150_v0__49_13="${__ret_get_download_path150_v0}"
-move_to_bin__151_v0 "${__ret_get_download_path150_v0__49_13}" "phpactor"
+get_download_path__152_v0 "phpactor/phpactor" 0
+ret_get_download_path152_v0__50_17="${ret_get_download_path152_v0}"
+move_to_bin__153_v0 "${ret_get_download_path152_v0__50_17}" "phpactor"
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
 echo "Install Typos LSP"
-get_download_path__150_v0 "tekumara/typos-lsp" 7
-__ret_get_download_path150_v0__52_17="${__ret_get_download_path150_v0}"
-download_to_bin__152_v0 "${__ret_get_download_path150_v0__52_17}" "typos-lsp" "typos.tar.gz"
+get_download_path__152_v0 "tekumara/typos-lsp" 7
+ret_get_download_path152_v0__53_21="${ret_get_download_path152_v0}"
+download_to_bin__154_v0 "${ret_get_download_path152_v0__53_21}" "typos-lsp" "typos.tar.gz"
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
 echo "Install Rust LSP"
-download_to_bin__152_v0 "https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz" "rust-analyzer" "rust-analyzer-x86_64-unknown-linux-gnu.gz"
+download_to_bin__154_v0 "https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz" "rust-analyzer" "rust-analyzer-x86_64-unknown-linux-gnu.gz"
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
 echo "Install GitLab CI LSP"
-get_download_path__150_v0 "alesbrelih/gitlab-ci-ls" 1
-__ret_get_download_path150_v0__58_13="${__ret_get_download_path150_v0}"
-move_to_bin__151_v0 "${__ret_get_download_path150_v0__58_13}" "x86_64-unknown-linux-gnu"
+get_download_path__152_v0 "alesbrelih/gitlab-ci-ls" 1
+ret_get_download_path152_v0__59_17="${ret_get_download_path152_v0}"
+move_to_bin__153_v0 "${ret_get_download_path152_v0__59_17}" "x86_64-unknown-linux-gnu"
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
 echo "Install HTMX LSP"
-get_download_path__150_v0 "ThePrimeagen/htmx-lsp" 2
-__ret_get_download_path150_v0__61_13="${__ret_get_download_path150_v0}"
-move_to_bin__151_v0 "${__ret_get_download_path150_v0__61_13}" "htmx-lsp"
+get_download_path__152_v0 "ThePrimeagen/htmx-lsp" 2
+ret_get_download_path152_v0__62_17="${ret_get_download_path152_v0}"
+move_to_bin__153_v0 "${ret_get_download_path152_v0__62_17}" "htmx-lsp"
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
 echo "Install Marksman LSP"
-get_download_path__150_v0 "artempyanykh/marksman" 1
-__ret_get_download_path150_v0__64_13="${__ret_get_download_path150_v0}"
-move_to_bin__151_v0 "${__ret_get_download_path150_v0__64_13}" "marksman"
+get_download_path__152_v0 "artempyanykh/marksman" 1
+ret_get_download_path152_v0__65_17="${ret_get_download_path152_v0}"
+move_to_bin__153_v0 "${ret_get_download_path152_v0__65_17}" "marksman"
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
 echo "Install Lua LSP"
-dir_exists__35_v0 "/opt/lua-language-server"
-__ret_dir_exists35_v0__67_8="${__ret_dir_exists35_v0}"
-if [ "$(( ! ${__ret_dir_exists35_v0__67_8} ))" != 0 ]; then
+dir_exists__36_v0 "/opt/lua-language-server"
+ret_dir_exists36_v0__68_12="${ret_dir_exists36_v0}"
+if [ "$(( ! ${ret_dir_exists36_v0__68_12} ))" != 0 ]; then
     cd "/opt/" || exit
     git clone https://github.com/LuaLS/lua-language-server
+    __status=$?
     cd "lua-language-server" || exit
 else
     cd "/opt/lua-language-server" || exit
 fi
 git pull >/dev/null 2>&1
+__status=$?
 ./make.sh >/dev/null 2>&1
 __status=$?
-symlink_create__40_v0 "/opt/lua-language-server/bin/lua-language-server" "/usr/local/bin/lua-language-server"
+symlink_create__41_v0 "/opt/lua-language-server/bin/lua-language-server" "/usr/local/bin/lua-language-server"
+__status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
 cd "/tmp" || exit
-__4_npm_lsp=("vscode-langservers-extracted" "@tailwindcss/language-server" "@olrtg/emmet-language-server" "intelephense" "bash-language-server")
-__5_npm_lsp_name=("CSS, HTML, JSON LSP" "Tailwind LSP" "Emmet LSP" "Intelephense LSP" "Bash LSP")
-__7_index=0;
-for __6_lsp in "${__4_npm_lsp[@]}"; do
-    echo "Install ${__5_npm_lsp_name[${__7_index}]}"
-    npm i -g "${__6_lsp}"
+npm_lsp_4=("vscode-langservers-extracted" "@tailwindcss/language-server" "@olrtg/emmet-language-server" "intelephense" "bash-language-server")
+npm_lsp_name_5=("CSS, HTML, JSON LSP" "Tailwind LSP" "Emmet LSP" "Intelephense LSP" "Bash LSP")
+index_7=0;
+for lsp_6 in "${npm_lsp_4[@]}"; do
+    echo "Install ${npm_lsp_name_5[${index_7}]}"
+    npm i -g "${lsp_6}"
     __status=$?
     if [ "${__status}" != 0 ]; then
         echo "Error"'!'" Exit code: ${__status}"
     fi
-    (( __7_index++ )) || true
+    (( index_7++ )) || true
 done
-__8_command_lsp=("pip install python-lsp-server" "gem install ruby-lsp")
-__9_command_lsp_name=("Python LSP" "Ruby LSP")
-__11_index=0;
-for __10_lsp in "${__8_command_lsp[@]}"; do
-    echo "Install ${__9_command_lsp_name[${__11_index}]}"
-    ${__10_lsp}
+command_lsp_8=("pip install python-lsp-server" "gem install ruby-lsp")
+command_lsp_name_9=("Python LSP" "Ruby LSP")
+index_11=0;
+for lsp_10 in "${command_lsp_8[@]}"; do
+    echo "Install ${command_lsp_name_9[${index_11}]}"
+    ${lsp_10}
     __status=$?
     if [ "${__status}" != 0 ]; then
         echo "Error"'!'" Exit code: ${__status}"
     fi
-    (( __11_index++ )) || true
+    (( index_11++ )) || true
 done
